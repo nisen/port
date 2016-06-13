edb1310265699d53c94a16dad84d26693557ce77
yahb5182 <yahb5182@163.com>
2016-06-13 11:46:11 +0800

    open

--- port.js
+++ port.js
@@ -1,6 +1,34 @@
-var SerialPort = require('serialport').SerialPort;
-var port = new SerialPort('com3',function writeAndDrain (data, callback) {
-        port.write(data, function () {
-        port.drain(callback);
+var SerialPort = require('C:/Program Files/nodejs/node_modules/serialport').SerialPort;
+var port = new SerialPort('com3');
+// port.close();
+// port.open();
+
+port.on('open', function() {
+    port.write('a', function(f) {
+        console.log(f);
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// SerialPort.list(function (err, ports) {
+//     ports.forEach(function(port) {
+//         console.log(port.comName);
+//         console.log(port.pnpId);
+//         console.log(port.manufacturer);
+//     });
+// });
+
+
+
+
+
